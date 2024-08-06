955cc5fa43bf532b4ef6eedda21988490233b6b8
Nour Maged <111568966+nourmag@users.noreply.github.com>
2024-08-06 17:07:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 import logging
 
 from src.pdf_handler import PDFHandler
-from src.embedding_handler import EmbeddingHandler
 from src.vector_store_handler import VectorStoreHandler
 from src.query_processor import QueryProcessor
 
@@ -19,16 +18,6 @@ def add_vertical_space(lines):
 # Load environment variables from .env file
 load_dotenv()
 
-# Ensure the OpenAI API key is set
-openai_api_key = os.getenv('OPENAI_API_KEY')
-if not openai_api_key:
-    st.error("The OpenAI API key must be set in the environment variables or in the .env file.")
-    st.stop()  # Stop the app if the API key is missing
-
-# Set the OpenAI API key in the environment
-os.environ['OPENAI_API_KEY'] = openai_api_key
-st.write(f"Using OpenAI API Key: {openai_api_key[:4]}...")
-
 # Main Streamlit app function
 def main():
     st.header("Chat with PDF 💬")
@@ -41,7 +30,7 @@ def main():
         This app is an LLM-powered chatbot built using:
         - [Streamlit](https://streamlit.io/)
         - [LangChain](https://python.langchain.com/)
-        - [OpenAI](https://platform.openai.com/docs/models) LLM model
+        - [Hugging Face](https://huggingface.co/) API
         ''')
         add_vertical_space(5)
         st.write('Made by Nour Maged & Naira Mohammed')
@@ -50,7 +39,6 @@ def main():
     pdf = st.file_uploader("Upload your PDF", type='pdf')
     if pdf is not None:
         pdf_handler = PDFHandler(pdf)
-        embedding_handler = EmbeddingHandler()
         vector_store_handler = VectorStoreHandler()
 
         try:
@@ -64,12 +52,7 @@ def main():
                 st.error("Failed to split the text into chunks. Please check the content of the PDF.")
                 return
 
-            embeddings = embedding_handler.generate_embeddings(chunks)
-            if not embeddings:
-                st.error("Failed to generate embeddings. Please check the content of the PDF.")
-                return
-
-            vector_store = vector_store_handler.store_embeddings(chunks, embeddings)
+            vector_store = vector_store_handler.store_embeddings(chunks)
             st.write('Embeddings Computation Completed.')
 
             # Accept user's questions/query
